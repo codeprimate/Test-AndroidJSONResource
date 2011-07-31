e7b81e29175e5fdde77eb2d3af77877be809733e
Patrick Morgan <patrick.morgan@amd.com>
2011-07-31 12:42:57 -0500

    * Created simple JSON provider.

---
+++ Server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '3fe3ae7fd932722f9ddaf7231df128d4610840f1f9151729a6970e1f17e53f0661dc90bf0697d2d097b0a188d7e8f5859862ea48b1278de5f4c7e3ed0d5c7937'
